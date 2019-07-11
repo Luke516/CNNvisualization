83b71b5907386c93350a71aea9c84360ca52d243
Luke <apple894894@gmail.com>
2019-07-12 01:22:10 +0800

    first working version

--- js/method1.js
+++ js/method1.js
@@ -11,7 +11,34 @@ function scaleToFit(canvas, img){
     return scale;
 }
 
+function flatten(arr) {
+    return arr.reduce(function (flat, toFlatten) {
+      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+}
+
+function getFilter(root, filterId) {
+    var out = [];
+    for(var i=0; i<h; i++){
+        for(var j=0; j<w; j++){
+            // console.log(i+','+j);
+            out.push(root[i][j][filterId]);
+            out.push(root[i][j][filterId]);
+            out.push(root[i][j][filterId]);
+            // out.push(100);
+            // out.push(100);
+            // out.push(100);
+            out.push(255);
+        }
+    }
+    return out;
+}
+
 $('#send').click(function(){
+    var outputDiv =  document.getElementById('output');
+    outputDiv.innerHTML = "";
+    document.getElementById("confirm").style.display = "none";
+    document.getElementById("loading").style.display = "flex";
     var canvas = document.querySelector("canvas");
     var ctx = canvas.getContext("2d");
     var pixels = [];
@@ -43,22 +70,92 @@ $('#send').click(function(){
     }
     var data = Object();
     data.signature_name = "activation";
-    data.instance = pixels;
+    data.instances = pixels;
 
     console.log(JSON.stringify(data));
-    // console.log(JSON.stringify(pixels));
+    console.log(JSON.stringify(pixels));
 
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: proxyurl + "http://140.114.85.24:8501/v1/models/model:predict",
-        contentType: 'application/json',
-        data: JSON.stringify(data),
-        success: function (msg) {
-            console.log(msg);
+
+    const httpHeaders = { 'Content-Type' : 'application/json'}
+    const myHeaders = new Headers(httpHeaders)
+    const url = proxyurl + "http://140.114.85.24:8501/v1/models/model2:predict";
+    const req = new Request(url, {method: 'POST', headers: myHeaders})
+
+    fetch(url, {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        // body: '{"signature_name":"activation"}'
+        body: JSON.stringify(data)
+    }).then(res=>res.json())
+    .then((res) => {
+        document.getElementById("confirm").style.display = "flex";
+        document.getElementById("loading").style.display = "none";
+        var root = res['predictions'][0];
+        console.log(root);
+        for(var key in root) {
+            var outputRow = document.createElement('div');
+            outputRow.setAttribute("class", "row");
+            outputDiv.appendChild(outputRow);
+            var outputTitle = key + " 的輸出 (前16個filter)："
+            var outputTitleElem = document.createElement('p');
+            outputTitleElem.innerHTML = outputTitle;
+            outputRow.appendChild(outputTitleElem);
+            outputRow.appendChild(document.createElement('br'));
+            if(root.hasOwnProperty(key)) {
+                //var flat = flatten(root[key]);
+                for(var i=0; i<16; i++){
+                    var flat = getFilter(root[key], i);
+                    console.log(flat);
+
+                    var new_canvas = document.createElement('canvas');
+                    new_canvas.id = key;
+                    new_canvas.width = w;
+                    new_canvas.height = h;
+                    new_canvas.style.zIndex = 8;
+                    new_canvas.style.position = "relative";
+                    new_canvas.style.border = "1px solid";
+                    //outputRow.appendChild(new_canvas);
+
+                    var ctx = new_canvas.getContext("2d");
+                    var resultImgData = ctx.createImageData(w, h);
+                    resultImgData.data.set(Uint8ClampedArray.from(flat));
+                    //QWQ
+                    console.log(resultImgData.width);
+                    console.log(resultImgData.height);
+                    console.log(resultImgData.data);
+                    ctx.putImageData(resultImgData,0,0);
+
+                    var outputUrl = new_canvas.toDataURL();
+                    imageFoo = document.createElement('img');
+                    imageFoo.src = outputUrl;
+                    imageFoo.style.maxWidth = '256px';
+                    imageFoo.style.maxHeight = '256px';
+                    outputRow.appendChild(imageFoo);
+                }
+                // break;
+            }
         }
-    });
+    });  
+
+    // $.ajax({
+    //     type: "POST",
+    //     url: "https://cors-anywhere.herokuapp.com/http://140.114.85.24:8501/v1/models/model:predict",
+    //     contentType: 'application/json',
+    //     data: JSON.stringify(data),
+    //     success: function (msg) {
+    //         console.log(msg);
+    //     },
+    //     error: function(XMLHttpRequest, textStatus, errorThrown) {
+    //         console.log(XMLHttpRequest.status);
+    //         console.log(XMLHttpRequest.readyState);
+    //         console.log(textStatus);
+    //         console.log(errorThrown)
+    //     }
+    // });
 })
 
 var main = function() {
